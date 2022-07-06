e6f592a413df5bfaefa30aef70c14d24583c9c92
Binoy <binoypatel14@gmail.com>
2022-07-05 23:18:05 -0400

    Website Upgrades

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -3,48 +3,61 @@ import { Section, TextBlock } from "../components";
 
 const EXPERIENCES = [
   {
-    company: "Blitz App, Inc",
+    company: "Blitz App, Inc.",
     title: "Product Data Analyst",
+    timePeriod: "Sept 2021 - July 2022",
     location: "San Francisco, CA",
-    timeRange: "Sept 2021 - Present",
     body: [
-      "Created BI reports enabling transparent and fast insights into business metrics, including operational efficiency, financial health and bug reporting.",
-      "Built data dashboards that measure company performance + player resonance",
-      "Guided future product strategy by discovering insights into player behavior",
-      "Improving operational excellence by learning insights into product quality",
-      "Informing business strategy with insights about competitors and the market landscape",
+      {
+        title: "Platform Analytics",
+        body: [
+          "Performed acquisition and behavioral cohort analysis, and created dashboards to track early retention issues and user churn trends.",
+          "Using SQL, Databricks & Tableau built comprehensive reporting pipelines for trackings insights and trends in user growth which resulted in easy-of-use tracking in overall company performance.",
+        ],
+      },
+      {
+        title: "Subscription Analytics",
+        body: [
+          "Automated reporting of Advance subscription metrics, to help the product team manage and understand subscription profitability and MRR, LTV, CAC, churn rates and more.",
+          "Partnered with the product team to drive product strategy and features through data-informed insights on comparing game engagement of pro vs non-pro users.",
+        ],
+      },
+      "<strong>Marketing Analytics</strong>: Designed KPIs for understanding Ad revenue in correlation to user journey, provided reports informing Ad touchpoints and placements ensuring marketing campaign success.",
+      "<strong>Product Sales Analytics</strong>: Designed interfaces to provide real-time overview of key business metrics and revenue helping depict the overall health of the product offering.",
+      "<strong>Data Engineering</strong>: Transformed and processed raw data for historic back-fills and automated daily events. Orchestrated a combination of notebooks, SQL, Spark to build ETL pipelines across large game player data sets (~100m new rows daily) to trigger daily workflows.",
     ],
   },
   {
-    company: "Okta, Inc",
+    company: "Okta, Inc.",
     title: "Analyst, Intern",
+    timePeriod: "Mar 2021 - Sept 2021",
     location: "San Francisco, CA",
-    timeRange: "Mar 2021 - Sept 2021",
     body: [
-      "Generating reports/dashboards on user search behaviors, product engagement insights to identify content gaps using Coveo Machine Learning, Google Analytics, Salesforce and providing recommendations to product strategy based on research for new content development",
-      "Implementing pipelines for data cleaning/manipulation using Python, SQL for ad-hoc analysis; Supporting launch of new product features and product testing by providing real-time analytics using Tableau based on internal/customer interactions",
+      "Generating reports/dashboards on user search behaviors, product engagement insights to identify content gaps using Coveo Machine Learning, Google Analytics, Salesforce to provide recommendations to product strategy based on research for new content development.",
+      "Implementing pipelines for data cleaning/manipulation using Python, SQL for ad-hoc analysis.",
+      "Supporting launch of new product features and product testing by providing real-time analytics using Tableau based on internal/customer interactions.",
     ],
   },
   {
     company: "Gilead Sciences, Inc.",
     title: "Data Scientist, Intern",
+    timePeriod: "Apr 2020 - Dec 2020",
     location: "Foster City, CA",
-    timeRange: "Apr 2020 - Dec 2020",
     body: [
-      "Analyzed over 2 million patient health claims, worked with data engineers to develop ETL workflows using Data Factory in Ms Azure",
-      "Wrote and optimized SQL queries to perform data extraction, developed data processing pipelines using python (Numpy, Pandas) and built ML models with 78% accuracy to identify target patients likely to switch on the new drug and predict quality target of drug market",
-      "Created effective data visualization dashboards used to model data with Tableau and Matplotlib and provided business insights to the Sales team for investing in the promotion of the product",
+      "Collaborated with data engineers to develop ETL workflows in Azure Data Factory to analyze over 2 million patient health claims.",
+      "Wrote and optimized SQL queries to perform data extraction, developed data processing pipelines using python (Numpy, Pandas) and built ML models with 78% accuracy to identify target patients likely to switch on the new drug and predict quality target of the drug market.",
+      "Created effective data visualization dashboards with Tableau and Matplotlib which provided business insights to the sales team for investing in the promotion of the product.",
     ],
   },
   {
-    company: "BioMarin Pharmaceutical, Inc",
+    company: "BioMarin Pharmaceutical, Inc.",
     title: "Data Science Analyst, Intern",
+    timePeriod: "Jun 2020 - Sept 2020",
     location: "San Rafael, CA",
-    timeRange: "Jun 2020 - Sept 2020",
     body: [
-      "Owned cleaning and processing of data with SQL, Excel, and Python (Pandas, NumPy) from multiple data sources to a data warehouse",
-      "Conceptualized and implemented PowerBI Dashboards for leadership team utilizing data to advise on strategic decisions on key business metrics like Promotion, Workforce Diversity, Employee Engagement, Office Capacity Planning, and Attrition",
-      "Independently designed KPIs on operational metrics in Python, PowerBI to analyze the gender and/or ethnicity pay gap",
+      "Led cleaning and processing of data with SQL, Python (Pandas, NumPy) from multiple data sources to a data warehouse.",
+      "Conceptualized and implemented PowerBI Dashboards for the leadership team utilizing data to advise on strategic decisions like Promotion, Workforce Diversity, Employee Engagement, Office Capacity Planning, and Attrition.",
+      "Independently designed KPIs on operational metrics in Python and PowerBI to analyze the gender and/or ethnicity pay gap.",
     ],
   },
 ];
@@ -62,14 +75,29 @@ const Experience: NextPage = () => {
                 <span>{e.company}</span>
               </h3>
               <h3 className="text-lg flex flex-col items-start sm:items-end">
-                <span>{e.timeRange}</span>
+                <span>{e.timePeriod}</span>
                 <span>{e.location}</span>
               </h3>
             </div>
             <ul className="list-disc ml-4">
-              {e.body.map((b, i) => (
-                <li key={i}>{b}</li>
-              ))}
+              {e.body.map((b, i) => {
+                if (typeof b === "object") {
+                  return (
+                    <li key={i}>
+                      <strong>{b.title}</strong>
+                      <ul className="list-disc ml-8">
+                        {b.body.map((b2, i2) => (
+                          <li
+                            key={i2}
+                            dangerouslySetInnerHTML={{ __html: b2 }}
+                          />
+                        ))}
+                      </ul>
+                    </li>
+                  );
+                }
+                return <li key={i} dangerouslySetInnerHTML={{ __html: b }} />;
+              })}
             </ul>
           </div>
         ))}
