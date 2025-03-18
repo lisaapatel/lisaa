44c7976a89ac6564405e1fe1df655a494ad43b6d
lisapatel98 <lisapatel1998@hotmail.com>
2025-03-17 17:31:29 -0700

    debug

--- next.config.js
+++ next.config.js
@@ -9,11 +9,6 @@ const nextConfig = {
   basePath: '/lisaa',
   assetPrefix: '/lisaa/',
   trailingSlash: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
 
 module.exports = nextConfig;
